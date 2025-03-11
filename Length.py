356512a15dfbea08dd653b9bb46478e380a050c0
Anna Bogatyreva <87075158+KerrLiGit@users.noreply.github.com>
2025-03-11 18:39:59 +0300

    Basic metrics

---
+++ Length.py
@@ -0,0 +1,15 @@
+class Length:
+    def meters_to_inch(self, a):
+        return a * 39.37
+
+    def kilometers_to_mile(self, a):
+        return a / 1.609
+
+    def meters_to_yard(self, a):
+        return a * 1.094
+
+    def meters_to_foots(self, a):
+        return a * 3.281
+
+    def kilometers_to_sea_mile(self, a):
+        return a / 1.852
---
+++ Weight.py
@@ -0,0 +1,12 @@
+class Weight:
+    def ton_to_english_ton(self, a):
+        return a / 1.016
+
+    def ton_to_american_ton(self, a):
+        return a * 1.102
+
+    def kilogram_to_lb(self, a):
+        return a * 2.205
+
+    def kilogram_to_ounce(self, a):
+        return a * 35.274
